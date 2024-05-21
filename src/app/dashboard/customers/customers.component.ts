b91d390cca36af50ef36e4184bcbe1fa6c59f576
achraf-bogryn <achrafbogryn12345@gmial.com>
2024-05-21 10:25:48 +0100

    model the admin dashborad

---
+++ src/app/dashboard/customers/customers.component.ts
@@ -0,0 +1,68 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {CustomersService} from "../services/customer.service";
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit{
+
+  public result : any[]=[];
+
+  constructor(
+    private router: Router,
+    private costumers:CustomersService,
+    private http: HttpClient
+  ){}
+
+  ngOnInit(): void {
+    this.costumers.getCostumer().subscribe(
+      (res:any)=>{
+
+        console.log(res);
+        this.result = res.customers;
+
+      },(err)=>{
+        console.log(err);
+      }
+    )
+  }
+
+  public formatPrice(price:any) {
+    if (typeof price === 'string') {
+
+      if (price.includes('$')) {
+
+        return price.replace('$', '') + '$';
+      } else {
+
+        return price + '$';
+      }
+    } else if (typeof price === 'number') {
+
+      return price.toString() + '$';
+    } else {
+
+      return 'N/A';
+    }
+  }
+
+  public  formatReadableDate(dateString:any) {
+    const options:any = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+
+    const date = new Date(dateString);
+
+    return date.toLocaleString('en-US', options);
+  }
+
+  public navigateTo(id:string){
+
+    this.router.navigate(['customers',id]);
+
+  }
+
+
+}

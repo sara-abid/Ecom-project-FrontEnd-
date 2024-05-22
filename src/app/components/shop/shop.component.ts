05695aec6863c199e8df991af80b04b5a3f859e3
sara <sara.abiid@gmail.com>
2024-05-22 14:58:01 +0000

    create sidebar

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit{
 
+  categorieOpen: boolean = false;
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    toggleCategorie(): void {
+        this.categorieOpen = !this.categorieOpen;
+    }
 }

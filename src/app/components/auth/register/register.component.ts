c5b6ebc89817cec6ee0d0e6a65b22c67d4a278c5
Mouad <mouad.elomari@um5r.ac.ma>
2024-06-04 17:09:53 +0100

    add register typesript

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/serviceApp/user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  // Définir les propriétés pour le formulaire
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(private userService: UserService) { }
+
+  register(form: any) {
+    const userData = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password
+    };
+
+
+
+    this.userService.registerUser(userData).subscribe(
+      response => {
+        console.log('Inscription réussie', response);
+        // Redirection ou affichage d'un message de succès
+        alert('Inscription réussie!');
+      },
+      error => {
+        console.error('Erreur lors de l\'inscription', error);
+      }
+    );
+  }
 }
+
+
